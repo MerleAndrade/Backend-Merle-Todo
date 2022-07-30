35da33f0274515eb3d9e2c00acab0b0a4cbddd84
merleandrade <andrade.merle@gmail.com>
2022-07-30 21:22:37 +0200

    Initial

---
+++ src/test/java/com/example/backendmerletodo/BackendMerleTodoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendmerletodo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendMerleTodoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
